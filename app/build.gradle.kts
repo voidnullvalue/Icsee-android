61a581e69d58af3c69b167b422df53ea1b69d7a2
voidnullvalue <voidnullvalue@users.noreply.github.com>
2026-07-07 18:20:10 +0000

    Bump to v0.11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.voidnullvalue.icseelocal"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.10.0"
+        versionCode = 11
+        versionName = "0.11.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
