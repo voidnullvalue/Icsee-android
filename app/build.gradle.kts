feb0a01c55ff61fc5aa793b68ab050148df316e4
|VOID| <rowlandkhd@gmail.com>
2026-08-24 15:19:10 -0500

    Bump version to 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.voidnullvalue.icseelocal"
         minSdk = 26
         targetSdk = 36
-        versionCode = 32
-        versionName = "1.1.1"
+        versionCode = 33
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
